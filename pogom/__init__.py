ced741567e352331ae51b333a2e62aa4f3750c2f
Darakath <darakath1128@gmail.com>
2016-07-26 19:18:52 -0400

    aaasd

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,9 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-<<<<<<< HEAD
     'GOOGLEMAPS_KEY': 'AIzaSyD7Q8x4QOS6XaEaQ3eEeYhmRfqsuylusXE'
-=======
-    'GOOGLEMAPS_KEY': 'GMAPS KEY HERE'
->>>>>>> refs/remotes/favll/master
 }
